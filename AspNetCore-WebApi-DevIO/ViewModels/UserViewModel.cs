5deb67fb79cb45d00bfa73648b382dda4e5ab0c7
Lara <laragalvanim@gmail.com>
2022-08-07 14:36:53 -0300

    Add UserViewModel

---
+++ AspNetCore-WebApi-DevIO/ViewModels/UserViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCore_WebApi_DevIO.ViewModels
+{
+    public class RegisterUserViewModel
+    {
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [EmailAddress(ErrorMessage = "The field {0} does not have a valid format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+		[StringLength(100, ErrorMessage = "The field {0} is mandatory and it should contain between {2} and {1} letters", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "The passwords are not equal")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginUserViewModel
+    {
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [EmailAddress(ErrorMessage = "The field {0} does not have a valid format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [StringLength(100, ErrorMessage = "The field {0} is mandatory and it should contain between {2} and {1} letters", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+
+    public class UserTokenViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<ClaimViewModel> Claims { get; set; }
+    }
+
+    public class LoginResponseViewModel
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public UserTokenViewModel UserToken { get; set; }
+    }
+
+    public class ClaimViewModel
+    {
+        public string Value { get; set; }
+        public string Type { get; set; }
+    }
+}
