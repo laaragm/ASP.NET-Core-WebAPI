10f94762fe3746d69fa07f8dbd90846856d9c1c3
Lara <laragalvanim@gmail.com>
2022-08-07 14:36:45 -0300

    Add AppSettings

---
+++ AspNetCore-WebApi-DevIO/Extensions/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNetCore_WebApi_DevIO.Extensions
+{
+	public class AppSettings
+	{
+		public string Secret { get; set; } // Encryption key
+		public int HoursToExpire { get; set; }
+		public string Issuer { get; set; }
+		public string Audience { get; set; } // Applications that should accept the token
+	}
+}
